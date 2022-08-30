a8f272a0e75c099092cc79aa56bee725a71c9e3f
Manny Del Real <manny.delreal@mailchimp.com>
2022-08-30 18:21:12 -0500

    Add folium map

--- app.py
+++ app.py
@@ -17,13 +17,28 @@ import seaborn as sns
 import matplotlib.pyplot as plt             # for plotting graphs
 from folium.plugins import MarkerCluster    # for visualising clusters
 
-import random
-
 ########### Define your variables ######
 
 tabtitle = 'F1 Data analysis'
 sourceurl = 'https://www.kaggle.com/datasets/rohanrao/formula-1-world-championship-1950-2020'
 githublink = 'https://github.com/manueldelreal/formula1-EDA'
+
+def create_map():
+    circuitsdf = pd.read_csv('race-data/circuits.csv')
+    circuits_folium = circuitsdf[['name', 'location', 'country', 'lat', 'lng']]
+    circuits_map = folium.Map(tiles="cartodbpositron")
+    marker_cluster = MarkerCluster().add_to(circuits_map)
+    for i in range(len(circuits_folium)):
+        lat = circuits_folium.iloc[i]['lat']
+        lng = circuits_folium.iloc[i]['lng']
+        radius = 5
+        popup_text = circuits_folium.iloc[i]['name']
+        folium.CircleMarker(location = [lat, lng], radius = radius,
+        popup = popup_text, fill = True).add_to(marker_cluster)
+        circuits_map.save('assets/circuits_map.html')
+    return circuits_map
+
+map_of_circuits = create_map()
 
 
 
@@ -35,35 +50,14 @@ app.title=tabtitle
 
 ########### Set up the layout
 
-app.layout = html.Div(children=[
-    html.H1('Visualising circuits around the world on a world map using clusters'),
-    html.A('Code on Github', href=githublink),
-    html.Br(),
-    html.A("Data Source", href=sourceurl),
-    ]
+app.layout = html.Div([
+    html.H1('Location of F1 circuits around the world'),
+    html.Iframe(id = 'map', srcDoc = open('assets/circuits_map.html', 'r').read(), width='100%', height='600')
+]
 )
 
 
 # make a function that can intake any varname and produce a map.
-
-def random_colors(number_of_colors):
-    color = ["#"+''.join([random.choice('0123456789ABCDEF') for j in range(6)])
-                 for i in range(number_of_colors)]
-    return color
-    
-circuitsdf = pd.read_csv('race-data/circuits.csv')
-circuits_folium = circuitsdf[['name', 'location', 'country', 'lat', 'lng']]
-
-circuits_map = folium.Map(tiles="cartodbpositron")
-marker_cluster = MarkerCluster().add_to(circuits_map)
-for i in range(len(circuits_folium)):
-    lat = circuits_folium.iloc[i]['lat']
-    lng = circuits_folium.iloc[i]['lng']
-    radius = 5
-    popup_text = circuits_folium.iloc[i]['name']
-    folium.CircleMarker(location = [lat, lng], radius = radius, 
-                        popup = popup_text, fill = True).add_to(marker_cluster)
-circuits_map
 
 
 ############ Deploy
